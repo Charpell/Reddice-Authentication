1efb5c9e67b2f9ecb34d1a7eb8b2e8c0c08333b7
Charpell <charpellumeh@gmail.com>
2018-03-24 13:28:29 +0100

    React router and basic navigation between pages

--- client/components/signup/SignupPage.js
+++ client/components/signup/SignupPage.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-export default class Greetings extends React.Component {
+export default class SignupPage extends React.Component {
 
   render() {
     return (
       <div className="jumbotron">
-        <h1>Hi!!</h1>
+        <h1>Sign up page</h1>
       </div>
     )
 
