39605372b7b83957e84362a15ab210133b5f191d
Charpell <charpellumeh@gmail.com>
2018-03-25 15:49:59 +0100

    Login and Logout on client

--- client/index.js
+++ client/index.js
@@ -5,8 +5,11 @@ import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from './rootReducer';
+import setAuthorizationToken from './utils/setAuthorizationToken'
+import jwt from 'jsonwebtoken';
 
 import routes from './routes';
+import { setCurrentUser } from './actions/authActions';
 
 const store = createStore(
   rootReducer,
@@ -15,6 +18,12 @@ const store = createStore(
     window.devToolsExtention ? window.devToolsExtention() : f => f
   )
 );
+
+if(localStorage.jwtToken) {
+  setAuthorizationToken(localStorage.jwtToken);
+  store.dispatch(setCurrentUser(jwt.decode(localStorage.jwtToken)))
+}
+
 
 render(
   <Provider store={store}>
