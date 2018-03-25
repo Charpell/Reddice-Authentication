476b5021ef4b490bad93736a9b7fa62bd6c322b5
Charpell <charpellumeh@gmail.com>
2018-03-25 11:10:38 +0100

    Create Login Form

---
+++ client/components/login/LoginPage.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import LoginForm from './LoginForm';
+// import { connect }  from 'react-redux';
+// import { userSignupRequest, isUserExists } from '../../actions/signupActions';
+// import { addFlashMessage } from '../../actions/flashMessages'
+
+class LoginPage extends React.Component {
+
+  render() {
+    // const { userSignupRequest, addFlashMessage, isUserExists } = this.props;
+    return (
+      <div className="row">
+        <div className="col-md-4 col-md-offset-4">
+          <LoginForm />
+        </div>
+      </div>
+    )
+
+  }
+}
+
+export default LoginPage;
+// SignupPage.propTypes = {
+//   userSignupRequest: React.PropTypes.func.isRequired,
+//   addFlashMessage: React.PropTypes.func.isRequired,
+//   isUserExists: React.PropTypes.func.isRequired
+// }
+
+// export default connect(null, { userSignupRequest, addFlashMessage, isUserExists })(SignupPage)
